9721c073e4206fc07827d8df58270ca152c3d181
Steve <steve.gackstetter@gmail.com>
2021-05-18 16:08:54 -0500

    Started using Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import './App.css';
 
-var total = 0;
-
-const add = () => {
-  total++;
-}
-
-
-function debouncedFunc(func: (...args: any[]) => void, time: number): Function {
-  let timeout: any;
-
-  return function executedFunction(this: any, ...args: any[]) {
-    var context = this;
-
-    var later = function () {
-      timeout = null;
-      func.call(context, ...args);
-    }
-
-    clearTimeout(timeout);
-
-    timeout = setTimeout(later, time);
-  }
-}
-
-let debouncedAdd = debouncedFunc(add, 2000);
 
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(count + 1);
+  }
+
+
+  function debouncedFunc(func: (...args: any[]) => void, time: number): Function {
+    let timeout: any;
+
+    return function executedFunction(this: any, ...args: any[]) {
+      var context = this;
+
+      var later = function () {
+        timeout = null;
+        func.call(context, ...args);
+      }
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(later, time);
+    }
+  }
+
+  let debouncedAdd = debouncedFunc(add, 2000);
   return (
     <div className="App">
       <button onClick={() => debouncedAdd()}>Debounced Add</button>
       <button onClick={add}>Regular Add</button>
-      <p>{total}</p>
+      <p>{count}</p>
     </div>
   );
 }
